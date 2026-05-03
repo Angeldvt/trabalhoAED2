67f5662986e90d2f0edc1e7d13b200451b0a2341
recebo <arthur.p.lag@gmail.com>
2026-05-03 15:48:56 -0300

    lista circular pura sla

--- config.py
+++ config.py
@@ -1,21 +1,25 @@
 #Inserir, Remover, Acessar, Exibir estrutura, Buscar informação, Acessar informação
 class Dado_da_Lista:
-    def __init__(self, dado=None, anterior=None, proximo=None):
+    def __init__(self, nodo, dado=None, anterior=None, proximo=None):
         self.dado_anterior = anterior
         self.dado_proximo = proximo
         self.dado = dado
+        self.nodo = nodo
     def alteraProximo(self, proximo):
         self.dado_proximo = proximo
     def alteraAnterior(self, anterior):
         self.dado_anterior = anterior
+    def alteraNodo(self, n):
+        self.nodo += n
 
 class Lista_circular:
     def __init__(self, n_max):
         self.maximo = n_max
 
+        
         self.lista = [None] * self.maximo
         for i in range(0, self.maximo):
-            self.lista[i] = Dado_da_Lista()
+            self.lista[i] = Dado_da_Lista(nodo=i+1)
         
         self.inicio = -1
         self.fim = -1
@@ -42,23 +46,36 @@ class Lista_circular:
 
 
     def inserir(self, dado, posicao: int=None):
+        if self.isCheia():
+            print('>>Lista cheia')
+            return False
+        
         if self.isVazia():
             if not posicao:
                 posicao = 0
             else:
                 posicao -= 1
-            self.lista[posicao] = Dado_da_Lista(dado)
+            self.lista[posicao] = Dado_da_Lista(nodo=posicao, dado=dado,)
             self.inicio = posicao
             self.fim = posicao
+            return True
 
         else:
             if posicao == None:
                 posicao = self.getTamanho()+1
                 print(posicao)
-                if (posicao-1) > 0:
-                    self.lista[posicao-1] = Dado_da_Lista(dado, anterior=self.lista[posicao-2], proximo=self.lista[posicao])
-                    self.lista[posicao-2].alteraProximo(self.lista[posicao-1])
-                self.fim += 1
+                #if (posicao-1) > 0: # não lembro o objetivo desse teste
+                if self.lista[posicao-1].dado == None:  # caso simples
+                    print("aqui tem nada")
+                    self.lista[posicao-1] = Dado_da_Lista(nodo=posicao, dado=dado, anterior=self.lista[posicao-2], proximo=self.lista[posicao])
+                    self.lista[posicao-2].alteraProximo(self.lista[posicao-1])  # faz o anterior apontar pro atual
+                    self.lista[posicao].alteraAnterior(self.lista[posicao-1])   # faz o proximo apontar pro atual
+                    self.fim += 1
+                    return True
+            
+            if posicao > 0:
+                pass
+
 
 
 
@@ -103,3 +120,9 @@ print(lista.inicio, lista.fim) # ---> quando tiver 1 elemento, ambos devem ter o
 print("item 2 (indice 1) da lista aponta pro dado anterior:", lista.lista[1].dado_anterior.dado)
 print("item 2 (indice 1) da lista aponta pro próximo dado:", lista.lista[1].dado_proximo.dado)
 print("item 1 (indice 0) da lista aponta pro próximo dado:", lista.lista[0].dado_proximo.dado)
+print("item 3 (indice 2) da lista aponta pro dado anterior:", lista.lista[2].dado_anterior.dado)
+
+
+lista.inserir(3)
+
+lista.exibir_estrutura()
